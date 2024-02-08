6731042b3d34a4a2d048bd458e9b687d423b3f7f
sukritgupta2024 <mailsukritgupta@gmail.com>
2024-02-08 16:26:03 +0530

    Minor change

--- src/orchestrator.py
+++ src/orchestrator.py
@@ -29,36 +29,6 @@ class Orchestrator:
         This function runs the analysis and generates the analysis report
         """
 
-        # analysis_result = ""
-        # ## Analysis1
-        # print(f'Analysis 1. {self.case_study.number_of_crashes_with_more_than_two_male_deaths()}')
-
-        # ## Analysis2
-        # print(f'Analysis 2. {self.case_study.number_of_two_wheelers_booked_for_crashes()}')
-
-        # ## Analysis3
-        # print(f'Analysis 3. {self.case_study.top_5_vehicle_makes_where_driver_dies_and_airbags_not_deployed()}')
-
-        # ## Analysis4
-        # print(f'Analysis 4. {self.case_study.number_of_vehicles_where_driver_has_valid_license_involved_in_hit_and_run()}')
-
-        # ## Analysis5
-        # print(f'Analysis 5. {self.case_study.state_with_highest_accidents_not_involving_females()}')
-        
-        # ## Analysis6
-        # print(f'Analysis 6. {self.case_study.top_third_to_top_fifth_vehicle_make_ids_with_highest_injury_death_count()}')
-
-        # ## Analysis7
-        # print(f'Analysis 7. {self.case_study.top_ethinicity_per_vehicle_body_type()}')
-
-        # ## Analysis8    
-        # print(f'Analysis 8. {self.case_study.top_5_zip_codes_with_alcohol_related_crashes()}')
-
-        # ## Analysis9
-        # print(f'Analysis 9. {self.case_study.count_of_distinct_crash_ids_where_no_damages_and_damage_level_greater_than_4_insurance_is_availed()}')
-        
-        # ## Analysis10
-        # print(f'Analysis 10. {self.case_study.get_top_5_vehicle_brand()}')
         analysis_result = ""
         analysis_methods = [
         self.case_study.number_of_crashes_with_more_than_two_male_deaths,
