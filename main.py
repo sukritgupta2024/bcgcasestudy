e212649cf3ed6e4181b345289b172e9822712ade
sukritgupta2024 <mailsukritgupta@gmail.com>
2024-02-08 22:19:21 +0530

    modified logger settings

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from pyspark.sql import SparkSession
 from src.orchestrator import Orchestrator
-
+import logging
 
 
 if __name__ == '__main__':
+        
+    logging.basicConfig(level=logging.ERROR)
+
     spark = SparkSession \
             .builder \
             .appName('USVehicleAccidentCaseStudy') \
             .getOrCreate()
     
+    spark.sparkContext.setLogLevel("ERROR")
+
     Orchestrator(spark)
-    spark.stop()
